8544248fa6d8c75619dc2a44726487e84989e248
Douglas Braga <dougbraga00@hotmail.com>
2021-10-18 19:45:32 -0300

    reorganização das pastas

---
+++ server.js
@@ -0,0 +1,15 @@
+//onst { APP_BASE_HREF } = require('@angular/common');
+const express = require( 'express');
+const path = require('path');
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + 'dist/app-estoque'));
+
+app.get('/*', (req, res)=>{
+    res.sendFile(__dirname + 'dist/app-estoque/index.html');
+});app.listen(PORT, () => {
+    console.log(PORT);
+})
