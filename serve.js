a4cacd361ab5af589c699eca2de22face5e7d645
Douglas Braga <dougbraga00@hotmail.com>
2021-10-18 22:26:07 -0300

    Serve update

--- serve.js
+++ serve.js
@@ -6,10 +6,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + 'dist/app-estoque'));
+app.use(express.static(__dirname + '/dist/app-estoque'));
 
 app.get('/*', (req, res)=>{
-    res.sendFile(__dirname + 'dist/app-estoque/index.html');
+    res.sendFile(__dirname + '/dist/app-estoque/index.html');
 });app.listen(PORT, () => {
     console.log(PORT);
 })
